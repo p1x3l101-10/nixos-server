8603582da3680e724c1a6bf7eef951cdbe7fa405
Pixel <scott.blatt.0b10@gmail.com>
2025-02-02 21:32:00 -0600

    fix lib

--- systems/x86_64-linux/pixels-server/disko-config.nix
+++ systems/x86_64-linux/pixels-server/disko-config.nix
@@ -1,6 +1,6 @@
 { inputs, ... }:
 
-inputs.self.lib.confTemplates.disko {
+inputs.nixos-home.lib.confTemplates.disko {
   disk-id = "nvme-WDC_PC_SN520_SDAPNUW-512G-1014_19131B802948_1";
   esp-size = "1G";
   swap-size = "50G";
