338bc3f5d90835b14e0f5b5777bbad61243c8f16
Pixel <scott.blatt.0b10@gmail.com>
2025-02-02 23:01:29 -0600

    fix inputs

--- flake.nix
+++ flake.nix
@@ -5,13 +5,22 @@
       url = "github:snowfallorg/lib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     impermanence.url = "github:nix-community/impermanence";
     nixos-home = {
       url = "github:p1x3l101-10/nixos";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         snowfall-lib.follows = "snowfall-lib";
-
+        lanzaboote.follows = "lanzaboote";
+        disko.follows = "disko";
         impermanence.follows = "impermanence";
         # Note: to keep in line with upstream, run this command to update lock file:
         # `nix flake update --inputs-from 'github:p1x3l101-10/nixos'`
