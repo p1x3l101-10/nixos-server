79a5a53d8bdb18865810c64850bed075b52a537e
Pixel <scott.blatt.0b10@gmail.com>
2025-02-03 10:11:51 -0600

    change sudo rules

--- modules/nixos/server/sudo.nix
+++ modules/nixos/server/sudo.nix
@@ -3,5 +3,16 @@
     security.sudo = {
         execWheelOnly = true;
         wheelNeedsPassword = false;
+        extraRules = [
+            {
+                users = [ "pixel" ];
+                commands = [
+                    {
+                        command = "ALL";
+                        options = [ "NOPASSWD" ];
+                    }
+                ];
+            }
+        ];
     };
 }
