594250af83aa2af2216ecf56509c7af909dac868
Pixel <scott.blatt.0b10@gmail.com>
2025-02-03 10:19:53 -0600

    begin key rotation

--- modules/nixos/server/proxy.nix
+++ modules/nixos/server/proxy.nix
@@ -4,7 +4,9 @@
   users.users.proxy = {
     isNormalUser = true;
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBXyvvhbuRPJyJgeaBCNiWI6IU1KP5WXrL2B1Qpvl36v sblatt@S-C1MSWH8RH3QF.normanps.norman.k12.ok.us"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMxvRlsW70GEkuqN/tdVM5X8EqdE6M/iA2iOHsA+HX8e sblatt@SS-Mac-sblatt.local" # MacBook
+      # NOTE: Make plans to phase out this key
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBXyvvhbuRPJyJgeaBCNiWI6IU1KP5WXrL2B1Qpvl36v sblatt@S-C1MSWH8RH3QF.normanps.norman.k12.ok.us" # Public-ish key
     ];
     shell = pkgs.shadow;
     home = "/var/empty";
