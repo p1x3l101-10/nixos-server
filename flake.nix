4185c8bd73193ec22811e1d88b5d651f5a90acb0
Pixel <scott.blatt.0b10@gmail.com>
2025-02-02 21:52:56 -0600

    remove dead inputs part 1

--- flake.nix
+++ flake.nix
@@ -5,28 +5,13 @@
       url = "github:snowfallorg/lib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     impermanence.url = "github:nix-community/impermanence";
-    nix-autobahn = {
-      url = "github:Lassulus/nix-autobahn";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nixos-home = {
       url = "github:p1x3l101-10/nixos";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         snowfall-lib.follows = "snowfall-lib";
-        lanzaboote.follows = "lanzaboote";
-        disko.follows = "disko";
         impermanence.follows = "impermanence";
-        nix-autobahn.follows = "nix-autobahn";
         # Note: to keep in line with upstream, run this command to update lock file:
         # `nix flake lock --recreate-lock-file --inputs-from 'github:p1x3l101-10/nixos'`
       };
