31f8fb00c3ea8360328f1337498dff471dafc8ce
Pixel <scott.blatt.0b10@gmail.com>
2025-02-04 17:35:08 -0600

    resolved

--- modules/nixos/server/network.nix
+++ modules/nixos/server/network.nix
@@ -14,6 +14,9 @@
   };
   services.resolved = {
     enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
     llmnr = "true";
     dnsovertls = "true";
   };
