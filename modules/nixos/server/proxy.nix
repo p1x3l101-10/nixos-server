7fd7f4d5644d7be1c95c92d716bbfd68cd231c96
Pixel <scott.blatt.0b10@gmail.com>
2025-02-04 09:49:28 -0600

    enforce key phase out

--- modules/nixos/server/proxy.nix
+++ modules/nixos/server/proxy.nix
@@ -6,7 +6,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMxvRlsW70GEkuqN/tdVM5X8EqdE6M/iA2iOHsA+HX8e sblatt@SS-Mac-sblatt.local" # MacBook
       # NOTE: Make plans to phase out this key
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBXyvvhbuRPJyJgeaBCNiWI6IU1KP5WXrL2B1Qpvl36v sblatt@S-C1MSWH8RH3QF.normanps.norman.k12.ok.us" # Public-ish key
+      #"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBXyvvhbuRPJyJgeaBCNiWI6IU1KP5WXrL2B1Qpvl36v sblatt@S-C1MSWH8RH3QF.normanps.norman.k12.ok.us" # Public-ish key
     ];
     shell = pkgs.shadow;
     home = "/var/empty";
