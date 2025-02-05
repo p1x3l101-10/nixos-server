f34f389142af839432be0c6b9bb510c4d5956600
Pixel <scott.blatt.0b10@gmail.com>
2025-02-05 09:16:44 -0600

    fix inputs

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
   inputs = {
     nixos-home.url = "github:p1x3l101-10/nixos";
-    nixpkgs.follows = "nixos-home";
-    snowfall-lib.follows = "nixos-home";
-    lanzaboote.follows = "nixos-home";
-    disko.follows = "nixos-home";
-    peerix.follows = "nixos-home";
-    impermanence.follows = "nixos-home";
+    nixpkgs.follows = "nixos-home/nixpkgs";
+    snowfall-lib.follows = "nixos-home/snowfall-lib";
+    lanzaboote.follows = "nixos-home/lanzaboote";
+    disko.follows = "nixos-home/disko";
+    peerix.follows = "nixos-home/peerix";
+    impermanence.follows = "nixos-home/impermanence";
   };
   outputs = inputs: import ./outputs.nix inputs;
 }
