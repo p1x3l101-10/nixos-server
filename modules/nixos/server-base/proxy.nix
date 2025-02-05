107138963dd12591673c075dc1511b3e8459536b
Pixel <scott.blatt.0b10@gmail.com>
2025-02-05 15:29:42 -0600

    remove comments

--- modules/nixos/server-base/proxy.nix
+++ modules/nixos/server-base/proxy.nix
@@ -6,8 +6,6 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMxvRlsW70GEkuqN/tdVM5X8EqdE6M/iA2iOHsA+HX8e sblatt@SS-Mac-sblatt.local" # MacBook
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJwbUnVjNdHpw4trfz2NEmg6Q901eO6cmELH2EilyFaV cbaxter@SS-Mac-cbaxter.local" # CJ
-      # NOTE: Make plans to phase out this key
-      #"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBXyvvhbuRPJyJgeaBCNiWI6IU1KP5WXrL2B1Qpvl36v sblatt@S-C1MSWH8RH3QF.normanps.norman.k12.ok.us" # Public-ish key
     ];
     shell = pkgs.shadow;
     home = "/var/empty";
