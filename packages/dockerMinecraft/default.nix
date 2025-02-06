34b1a9059e42d12eab2d870ffe92b7095d71d05d
Pixel <scott.blatt.0b10@gmail.com>
2025-02-06 10:44:27 -0600

    fix the damn hash

--- packages/dockerMinecraft/default.nix
+++ packages/dockerMinecraft/default.nix
@@ -2,7 +2,7 @@
 , dockerTools
 , imageName ? "itzg/minecraft-server"
 , imageDigest ? "sha256:9fcc91f052c47c5da7ae280b7cd1603ee7d657867c19fe4d6b5260edfd72db30"
-, sha256 ? "05yzshim40bniqnhzv708px0s4rxc6fkncdnsyixcgsji1zncp8i"
+, sha256 ? "sha256-VzIfANTMDH3dyc5Icju1b+M9yG8LHyoQdzu4MxwAWS0="
 }:
 
 dockerTools.pullImage {
