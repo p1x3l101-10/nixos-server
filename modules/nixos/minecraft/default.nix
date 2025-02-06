f3856caad8d72edf014a4020b2906cac1c7921fb
Pixel <scott.blatt.0b10@gmail.com>
2025-02-06 10:39:40 -0600

    setup a *very* basic minecraft server config

---
+++ modules/nixos/minecraft/default.nix
@@ -0,0 +1,43 @@
+{ pkgs, lib, ... }:
+let
+  curseforge-api-key = "$2a$10$bL4bIL5pUWqfcO7KQtnMReakwtfHbNKh6v1uTpKlzhwoueEJQnPnm";
+in {
+  virtualisation.oci-containers.containers.minecraft = {
+    environment = {
+       EULA = "TRUE";
+       CF_API_KEY = curseforge-api-key;
+       TYPE = "AUTO_CURSEFORGE";
+       CF_SLUG = "terrafirmagreg-modern";
+       CF_FILE_ID = "6123835";
+       MODRINTH_PROJECTS = ''
+        tomstfg-integration:b7OlDn35
+        toms-storage:UjCLHkAa
+       '' + ''
+        iatfg-integration:jXoVQDSZ
+        immersive-aircraft:BuPtsmaI
+        man-of-many-planes:uZNB5Zrk
+       '';
+    };
+    image = "internal/docker-minecraft:latest";
+    imageFile = pkgs.internal.dockerMinecraft;
+    ports = [
+      "25565:25565"
+    ];
+    volumes = [
+      "/var/lib/minecraft/data:/data:rw"
+    ];
+    autoStart = true;
+  };
+  systemd.tmpfiles.settings."10-minecraft" = {
+    "/var/lib/minecraft".d = {
+      user = "root";
+      group = "root";
+      mode = "0755";
+    };
+    "/var/lib/minecraft/data".d = {
+      user = "root";
+      group = "root";
+      mode = "0755";
+    };
+  };
+}
---
+++ packages/dockerMinecraft/default.nix
@@ -0,0 +1,14 @@
+{ lib
+, dockerTools
+, imageName ? "itzg/minecraft-server"
+, imageDigest ? "sha256:9fcc91f052c47c5da7ae280b7cd1603ee7d657867c19fe4d6b5260edfd72db30"
+, sha256 ? "05yzshim40bniqnhzv708px0s4rxc6fkncdnsyixcgsji1zncp8i"
+}:
+
+dockerTools.pullImage {
+  inherit imageName imageDigest sha256;
+  finalImageName = "internal/docker-minecraft";
+  finalImageTag = "latest";
+  os = "linux";
+  arch = "x86_64";
+}
