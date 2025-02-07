df66570e34c8d44cdec85a026a59b360d56f7b18
Pixel <scott.blatt.0b10@gmail.com>
2025-02-07 15:14:37 -0600

    change back

--- modules/nixos/terraria/default.nix
+++ modules/nixos/terraria/default.nix
@@ -6,7 +6,7 @@
     openFirewall = true;
     difficulty = "journey";
     world = {
-      name = "The_Dirty_End_of_Spurs";
+      name = "Gallifrey";
       seed = "for the worthy";
     };
     journey.setDifficulty = "all"; # Temp for setting master
