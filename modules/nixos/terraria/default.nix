faef8d37ef5bb732191bd5790d8ff24ddacd8784
Pixel <scott.blatt.0b10@gmail.com>
2025-02-07 10:58:46 -0600

    change to journey world

--- modules/nixos/terraria/default.nix
+++ modules/nixos/terraria/default.nix
@@ -4,10 +4,14 @@
   services.tmodloader = {
     enable = true;
     openFirewall = true;
-    difficulty = "expert";
+    difficulty = "journey";
     world = {
-      name = "Bacchus";
-      seed = "1220350133";
+      name = "Gallifrey";
+      seed = "for the worthy";
+    };
+    journey = {
+      setDifficulty = "all";
+      spawnRate = "all";
     };
     port = 24454;
     mods.enabled = [
@@ -16,39 +20,25 @@
       2817254924 # Gensokyo
       2824688072 # Calamity Mod
       2563862309 # The Stars Above
-      2909886416 # Thorium
       2838015851 # Catalyst Mod
-      2563309347 # Magic Storage
       ## Small mods
       2973922820 # Touhou Little Friends ~ Adventure with cute partners
       2824688804 # Calamity's Vanities
       2995193002 # Calamity: Wrath of the Gods
-      3071240652 # Automation & Defense
-      3030483105 # Enchanted Moons
       2669644269 # Boss Checklist
       2565639705 # Ore Excavator (1.4.3/1.4.4 Veinminer)
       2815010161 # Shared World Map
       2877850919 # Smarter Cursor
       2563082541 # Angler Shop
-      2619954303 # Recipe Browser
       2687866031 # Census - Town NPC Checklist
       2599842771 # AlchemistNPC Lite
-      3227702022 # Bound NPC Immunity
       3024992349 # Better zenith in Calamity
-      2706139083 # Craftable Accessories
-      2825810436 # Craftable
       2974503494 # More Pylons
       2860270524 # Calamity Ranger Expansion
       3246145263 # Calamity Texture Pack Glowmask Fix
       2827162563 # Improved Item Info
-      2821625825 # Golden Trough
       3124369320 # More Trophies and Relics [Modded Support]
-      3165492818 # Auto Fisher
-      3008058007 # Nurse Overhaul
       3346854898 # Auto Reforge Golden Fix
-      3402595565 # Multiworld Pylon
-      3346335121 # Cursor Bug Net
-      3407770135 # Guaranteed Traveling Merchant
       2979448082 # Summoner UI (Latest Stable Release)
       2896816354 # Unusacies' Battle Rods
       2812377597 # No Pylon Restrictions
@@ -58,21 +48,20 @@
       2992213994 # Instant Platform Fallthrough
       2710494433 # Too Many Accessories
       2907520140 # The Kill Bind Mod
+      2990764001 # Shimmer Extra
+      3058372966 # Aether's Blessing: Shimmer QoL
+      2767965680 # Tome of Research Sharing
       ## Music Mods
       2824688266 # Calamity Mod Music
       2816188633 # Vanilla Calamity Mod Music
 
       # Library Mods
       2785100219 # Subworld Library
-      2908170107 # absoluteAquarian Utilities (SerousCommonLib)
       3222493606 # Luminance
 
       # Compat Mods
-      3106201538 # Thorium + Calamity Convergence Mod REDUX
-      2816999612 # Recipe Browser && Magic Storage
       ## Weapon Scaling to make things work w/ calamity endgame
       2571636086 # W1K's Weapon Scaling
-      2940199906 # Weapon Scaling Thorium Patch
       3133081405 # W1KScaling Gensokyo Patch
       3199703539 # W1k's Scaling Calamity Patch
     ];
@@ -108,7 +97,6 @@
           { Mod = "EvilPylon"; Name = "CrimsonPylon"; DisplayName = "Crimson Pylon"; }
           { Mod = "EvilPylon"; Name = "HellPylon"; DisplayName = "Hell Pylon"; }
           { Mod = "EvilPylon"; Name = "CloudPylon"; DisplayName = "Cloud Pylon"; }
-          { Mod = "MultiworldPylon"; Name = "MultiworldPylonItem"; DisplayName = "Multiworld Pylon"; }
         ];
       };
     };
