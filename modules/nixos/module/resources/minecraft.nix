eb53aa937f8aabf55ec5191d2a9fdb5f27e8b4ad
Pixel <scott.blatt.0b10@gmail.com>
2025-02-08 14:40:31 -0600

    add hashes

--- modules/nixos/module/resources/minecraft.nix
+++ modules/nixos/module/resources/minecraft.nix
@@ -4,17 +4,17 @@
     "8" = {
       # java8
       imageDigest = "sha256:b70787a42a14867669d66c900b5c42bbaf9d2a21c7e0edd29b9c0e050c95b6ea";
-      sha256 = "";
+      sha256 = "0scsmi52x8wq2sxqf360116s8w4f3ywp9k32akwxhwnzq4nfjd67";
     };
     "17" = {
       # java17-alpine
       imageDigest = "sha256:c7f2bd3d6cc4836c2a14cab0d1e4776e891c1fc87772ed7bbc8f66d61300caab";
-      sha256 = "";
+      sha256 = "0g9jgcnb3nz8ywdfl6p4d443c21ngqnqaslc9291inwb9f3svshh";
     };
     "21" = {
       # java21-alpine
       imageDigest = "sha256:fd43d03de558b598401968f898b6976ef05875234153261e7772470dce834c11";
-      sha256 = "";
+      sha256 = "0virk1y9qps45sqm5c5pg282nykl9srfc06h3l35ahx6idr3cz38";
     };
   };
 }
