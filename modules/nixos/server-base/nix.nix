9e4ffd8af6800da3d79b4f40a6028bf789b70e6b
Pixel <scott.blatt.0b10@gmail.com>
2025-02-07 21:07:47 -0600

    shut up

--- modules/nixos/server-base/nix.nix
+++ modules/nixos/server-base/nix.nix
@@ -6,6 +6,8 @@
     substitute = false;
     substituters = lib.mkForce [];
     extra-substituters = lib.mkForce [];
-    registry = inputs.nixos-home.lib.confTemplates.registry inputs;
+    registry = (inputs.nixos-home.lib.confTemplates.registry (builtins.removeAttrs inputs ["disko"])) // {
+      disko.flake = inputs.disko; # Prints out so many warnings on eval
+    };
   };
 }
