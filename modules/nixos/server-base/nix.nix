ce893c66edacd5ce18cabd299feb93dd7a2d4da1
Pixel <scott.blatt.0b10@gmail.com>
2025-02-07 21:09:18 -0600

    fix registry

--- modules/nixos/server-base/nix.nix
+++ modules/nixos/server-base/nix.nix
@@ -6,8 +6,6 @@
     substitute = false;
     substituters = lib.mkForce [];
     extra-substituters = lib.mkForce [];
-    registry = (inputs.nixos-home.lib.confTemplates.registry (builtins.removeAttrs inputs ["disko"])) // {
-      disko.flake = inputs.disko; # Prints out so many warnings on eval
-    };
   };
+  nix.registry = inputs.nixos-home.lib.confTemplates.registry inputs;
 }
