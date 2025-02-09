c2a80199eb3561e27d82a29661b8c9e315c9679a
Pixel <scott.blatt.0b10@gmail.com>
2025-02-08 19:43:02 -0600

    I suck at my job

--- modules/nixos/module/minecraft.nix
+++ modules/nixos/module/minecraft.nix
@@ -164,7 +164,7 @@ in
         cfg.settings.extraEnv
         (mkEnv "EULA" (builtins.toString cfg.settings.eula))
         # Modrinth mod list
-        (mkEnvRawList "MODRINTH_PROJECTS" (lib.forEach (x: lib.internal.minecraft.translateModName x) cfg.modrinth.mods.projects) "\n")
+        (mkEnvRawList "MODRINTH_PROJECTS" (lib.forEach cfg.modrinth.mods.projects (x: lib.internal.minecraft.translateModName x)) "\n")
         # Type and version
         (mkEnv "VERSION" cfg.settings.version)
         (mkEnv "TYPE" cfg.settings.type)
