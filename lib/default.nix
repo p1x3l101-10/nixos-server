6f8afdecec134daebf52e0637184945246aa5bfe
Pixel <scott.blatt.0b10@gmail.com>
2025-02-08 22:04:19 -0600

    dedupe

--- lib/default.nix
+++ lib/default.nix
@@ -2,7 +2,7 @@
 
 let
   inherit (lib.nixos-home.internal) genLib callLibPrimitive;
-  genLib1 = lib.nixos-home.internal.genLib callLibPrimitive;
+  genLib1 = genLib callLibPrimitive;
 in genLib1 ./. [
   "minecraft"
 ]
