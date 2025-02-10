135d5b36d1ea5aade6a6266dacc15d2d4b94096b
Pixel <scott.blatt.0b10@gmail.com>
2025-02-10 11:48:10 -0600

    why vscode, whyyyy

--- modules/nixos/nextcloud/default.nix
+++ modules/nixos/nextcloud/default.nix
@@ -44,7 +44,7 @@ in {
       systemd.tmpfiles.settings = {
         "50-host-state"."/nix/host/state/Servers/NextCloud".d = { mode = "0755"; };
         "50-host-keys"."/nix/host/keys/nextcloud".d = { mode = "0700"; };
-      }
+      };
     }
   ]);
 }
