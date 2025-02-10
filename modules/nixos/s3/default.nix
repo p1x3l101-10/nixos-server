db30d43ca1eb698031e42f6873d98e444f7d3168
Pixel <scott.blatt.0b10@gmail.com>
2025-02-10 11:00:52 -0600

    move tmpfiles settings to host

--- modules/nixos/s3/default.nix
+++ modules/nixos/s3/default.nix
@@ -5,20 +5,25 @@
 let
   # Habit
   cfg.enable = true;
-in {
-  containers.s3 = lib.modules.mkIf cfg.enable {
-    autoStart = true;
-    privateNetwork = true;
-    localAddress = "10.10.10.3/24";
-    hostBridge = "br0";
-    ephemeral = true;
-    bindMounts = {
-      "/nix/host/keys/minio".isReadOnly = false;
-      "/nix/host/state/Servers/Minio" = {
-        isReadOnly = false;
-        mountPoint = "/var/lib/minio";
+in lib.nixos-home.attrsets.mergeAttrs [
+  {
+    containers.s3 = lib.modules.mkIf cfg.enable {
+      autoStart = true;
+      privateNetwork = true;
+      localAddress = "10.10.10.3/24";
+      hostBridge = "br0";
+      ephemeral = true;
+      bindMounts = {
+        "/nix/host/keys/minio".isReadOnly = false;
+        "/nix/host/state/Servers/Minio" = {
+          isReadOnly = false;
+          mountPoint = "/var/lib/minio";
+        };
       };
+      config = { ... }: { imports = [ ./container.nix ]; };
     };
-    config = { ... }: { imports = [ ./container.nix ]; };
-  };
-}
+  }
+  lib.internal.sss.makeTmpFiles [
+  "nextcloud"
+  ]
+]
