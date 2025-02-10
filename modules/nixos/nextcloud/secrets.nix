838c2406ac48415af85b6edc2c48941b913a4910
Pixel <scott.blatt.0b10@gmail.com>
2025-02-10 17:32:23 -0600

    preset empty secrets file

--- modules/nixos/nextcloud/secrets.nix
+++ modules/nixos/nextcloud/secrets.nix
@@ -2,4 +2,10 @@
 
 {
   services.nextcloud.secretFile = "/run/keys/nextcloud/secrets.json";
+  systemd.tmpfiles.settings."11-preset-key"."/run/keys/nextcloud/secrets.json".f = {
+    user = "root";
+    group = "root";
+    mode = "0400";
+    argument = "{}";
+  };
 }
