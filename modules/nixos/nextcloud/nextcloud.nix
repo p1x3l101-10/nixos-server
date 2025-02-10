5498d17a9d7bc84262acba473c00aed8f908cfb6
Pixel <scott.blatt.0b10@gmail.com>
2025-02-10 10:43:28 -0600

    update nextcloud version

--- modules/nixos/nextcloud/nextcloud.nix
+++ modules/nixos/nextcloud/nextcloud.nix
@@ -3,7 +3,7 @@
 {
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud28;
+    package = pkgs.nextcloud30;
     hostName = "166.113.94.161";
     datadir = "/var/lib/nextcloud";
     config = {
