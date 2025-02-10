224ded5dc98f3d7b529b24e72247dd09fe034135
Pixel <scott.blatt.0b10@gmail.com>
2025-02-10 11:35:24 -0600

    tmpfiles

--- modules/nixos/s3/default.nix
+++ modules/nixos/s3/default.nix
@@ -5,26 +5,31 @@
 let
   # Habit
   cfg.enable = true;
-in lib.nixos-home.attrsets.mergeAttrs [
-  {
-    containers.s3 = lib.modules.mkIf cfg.enable {
-      autoStart = true;
-      privateNetwork = true;
-      localAddress = "10.10.10.3/24";
-      hostBridge = "br0";
-      ephemeral = true;
-      bindMounts = {
-        "/nix/host/keys/minio".isReadOnly = false;
-        "/nix/host/state/Servers/Minio" = {
-          isReadOnly = false;
-          mountPoint = "/var/lib/minio";
-          hostPath = "/nix/host/state/Servers/Minio";
+in {
+  config = lib.modules.mkIf cfg.enable lib.nixos-home.attrsets.mergeAttrs [
+    {
+      containers.s3 = {
+        autoStart = true;
+        privateNetwork = true;
+        localAddress = "10.10.10.3/24";
+        hostBridge = "br0";
+        ephemeral = true;
+        bindMounts = {
+          "/nix/host/keys/minio".isReadOnly = false;
+          "/nix/host/state/Servers/Minio" = {
+            isReadOnly = false;
+            mountPoint = "/var/lib/minio";
+            hostPath = "/nix/host/state/Servers/Minio";
+          };
         };
+        config = { ... }: { imports = [ ./container.nix ]; };
       };
-      config = { ... }: { imports = [ ./container.nix ]; };
-    };
-  }
-  (lib.internal.sss.makeTmpFiles [
-    "nextcloud"
-  ])
-]
+    }
+    {
+      systemd.tmpfiles.settings."50-host-state"."/nix/host/state/Servers/Minio".d = { mode = "0755"; };
+    }
+    (lib.internal.sss.makeTmpFiles [
+      "nextcloud"
+    ])
+  ]
+}
