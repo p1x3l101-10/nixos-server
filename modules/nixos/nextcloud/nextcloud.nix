74e5da75380accc44352d55b5b4a6b62e04eed3a
Pixel <scott.blatt.0b10@gmail.com>
2025-02-11 09:14:40 -0600

    list 2 electric boogaloo

--- modules/nixos/nextcloud/nextcloud.nix
+++ modules/nixos/nextcloud/nextcloud.nix
@@ -15,7 +15,7 @@
   };
   # Make adminPass readable by nextcloud:nextcloud
   systemd.services."nextcloud-setup-setup" = {
-    requiredBy = "nextcloud-setup.service";
+    requiredBy = [ "nextcloud-setup.service" ];
     before = [ "nextcloud-setup.service" ];
     script = ''
       cp /run/keys/nextcloud/admin-password.txt /tmp/nextcloud-admin-password.txt
