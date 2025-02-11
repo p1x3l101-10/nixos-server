16f025aa0b88346a42133fba8bec5b73bf9a8586
Pixel <scott.blatt.0b10@gmail.com>
2025-02-11 09:18:00 -0600

    change secret

--- modules/nixos/nextcloud/secrets.nix
+++ modules/nixos/nextcloud/secrets.nix
@@ -3,9 +3,9 @@
 {
   services.nextcloud.secretFile = "/run/keys/nextcloud/secrets.json";
   systemd.tmpfiles.settings."11-preset-key"."/run/keys/nextcloud/secrets.json".f = {
-    user = "root";
-    group = "root";
-    mode = "0400";
+    user = "nextcloud";
+    group = "nextcloud";
+    mode = "0440";
     argument = "{}";
   };
 }
