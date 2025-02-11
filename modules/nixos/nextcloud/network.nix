2263139b864543cf725893cc83de3770b80165a6
Pixel <scott.blatt.0b10@gmail.com>
2025-02-11 10:30:06 -0600

    lists

--- modules/nixos/nextcloud/network.nix
+++ modules/nixos/nextcloud/network.nix
@@ -8,7 +8,7 @@
       "40-eth0" = {
         name = "eth0";
         linkConfig.RequiredForOnline = "routable";
-        gateway = "10.10.10.1";
+        gateway = [ "10.10.10.1" ];
         address = [ "10.10.10.4/24" ];
       };
     };
