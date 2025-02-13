bca581b970c0eca14fc89d414532d4cb19be02dc
Pixel <scott.blatt.0b10@gmail.com>
2025-02-13 15:18:09 -0600

    try no file url

--- modules/nixos/minecraft/default.nix
+++ modules/nixos/minecraft/default.nix
@@ -13,7 +13,7 @@ in
       memory = 8;
       port = 25565;
       extraEnv = {
-        GENERIC_PACK = "file://" + (pkgs.callPackage ./genericPack.nix {}) + "/pack.zip";
+        GENERIC_PACK = (pkgs.callPackage ./genericPack.nix {}) + "/pack.zip";
       };
       rconStartup = [];
     };
