f4b0a49f1dc029801b3bef6c27b0f89ab6629eef
Pixel <scott.blatt.0b10@gmail.com>
2025-02-13 18:21:57 -0600

    lock versions

---
+++ modules/nixos/module/resources/mc-versions/17-alpine.nix
@@ -0,0 +1,7 @@
+{
+  imageName = "docker.io/itzg/minecraft-server";
+  imageDigest = "sha256:45785ba6ffe600b46cd2ccfe4ae92b16f83b01ae367202d6b78011e6f6ea1659";
+  sha256 = "1320pzfl5mp8dbapw46yqgd31z44y9lgh24hx6lll1f7sdgp4dk9";
+  finalImageName = "docker.io/itzg/minecraft-server";
+  finalImageTag = "java17-alpine";
+}
