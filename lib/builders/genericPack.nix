6e88e31b07b9e7667b1fefc11136824b9309d816
Pixel <scott.blatt.0b10@gmail.com>
2025-02-16 23:10:53 -0600

    fix out perms

--- lib/builders/genericPack.nix
+++ lib/builders/genericPack.nix
@@ -25,10 +25,11 @@ stdenv.mkDerivation {
     mkdir -p ./config ./mods
     touch ./config/.keep
     touch ./mods/.keep
-    umask 007
+    umask 002
     tar cvhzf ./out.tar.gz --no-same-permissions --no-same-owner ./*
   '';
   installPhase = ''
     mv ./out.tar.gz $out
+    chmod 644 $out
   '';
 }
