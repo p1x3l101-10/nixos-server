7864159f17ff9f02c16bd8ad32201256abcfef4e
Pixel <scott.blatt.0b10@gmail.com>
2025-02-17 15:55:16 -0600

    no unpack

--- lib/builders/genericMod.nix
+++ lib/builders/genericMod.nix
@@ -10,6 +10,7 @@ stdenv.mkDerivation {
   inherit name version;
   src = file;
   sourceRoot = ".";
+  unpackPhase = '''';
   buildPhase = ''
     mkdir -p mods
     mv ${file.name} "./mods/${file.name}"
