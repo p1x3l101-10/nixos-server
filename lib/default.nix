d4af653b4e915a12ae0edfa3541626f6ca5ef26b
Pixel <scott.blatt.0b10@gmail.com>
2025-02-16 22:13:00 -0600

    maybe include the thing

--- lib/default.nix
+++ lib/default.nix
@@ -4,6 +4,7 @@ let
   inherit (lib.nixos-home.internal) genLib callLibPrimitive;
   genLib1 = genLib callLibPrimitive;
 in genLib1 ./. [
+  "builders"
   "minecraft"
   "sss"
 ]
--- modules/nixos/minecraft/default.nix
+++ modules/nixos/minecraft/default.nix
@@ -1,11 +1,11 @@
-{ pkgs, lib, namespace, ... }:
+{ pkgs, lib, ... }:
 let
   curseforge-api-key = "$2a$10$bL4bIL5pUWqfcO7KQtnMReakwtfHbNKh6v1uTpKlzhwoueEJQnPnm";
 in
 {
   services.minecraft = {
     enable = true;
-    generic.pack = builtins.toString (lib.${namespace}.builders.genericPack {
+    generic.pack = builtins.toString (lib.internal.builders.genericPack {
       packList = [
         ./overrides
         pkgs.fetchurl {
