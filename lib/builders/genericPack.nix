ed9d8a988c4d6bede71253893c2277e9966008e5
Pixel <scott.blatt.0b10@gmail.com>
2025-02-16 23:06:53 -0600

    change umask

--- lib/builders/genericPack.nix
+++ lib/builders/genericPack.nix
@@ -25,7 +25,7 @@ stdenv.mkDerivation {
     mkdir -p ./config ./mods
     touch ./config/.keep
     touch ./mods/.keep
-    umask 002
+    umask 007
     tar cvhzf ./out.tar.gz --no-same-permissions --no-same-owner ./*
   '';
   installPhase = ''
