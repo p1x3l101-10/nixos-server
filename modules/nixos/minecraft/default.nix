d363e9f92308010d68944e4bebd2dacde387f11f
Pixel <scott.blatt.0b10@gmail.com>
2025-02-16 22:18:38 -0600

    extract file

--- modules/nixos/minecraft/default.nix
+++ modules/nixos/minecraft/default.nix
@@ -8,7 +8,7 @@ in
     generic.pack = builtins.toString (lib.internal.builders.genericPack {
       packList = [
         ./overrides
-        (pkgs.fetchurl {
+        (pkgs.fetchzip {
           url = "https://downloads.gtnewhorizons.com/ServerPacks/GT_New_Horizons_2.7.2_Server_Java_17-21.zip";
           hash = "sha256-IDf53ScNurrewUBbAw5McmzXXuCyRbLs+F0ObY3wUlg=";
         })
