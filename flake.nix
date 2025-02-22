d5d28a97eaa0bad29d5ef3dae1112aa1b4ab4f58
Pixel <scott.blatt.0b10@gmail.com>
2025-02-22 00:55:10 -0600

    add unfree nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixos-home.url = "github:p1x3l101-10/nixos";
     nixpkgs.follows = "nixos-home/nixpkgs";
     snowfall-lib.follows = "nixos-home/snowfall-lib";
+    nixpkgs-unfree.follows = "nixos-home/nixpkgs-unfree";
     lanzaboote.follows = "nixos-home/lanzaboote";
     disko.follows = "nixos-home/disko";
     impermanence.follows = "nixos-home/impermanence";
