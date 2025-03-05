158341ce19249404ee3ee690009a1521a5d130f6
Pixel <scott.blatt.0b10@gmail.com>
2025-03-05 12:35:01 -0600

    add user

--- modules/nixos/server-base/proxy.nix
+++ modules/nixos/server-base/proxy.nix
@@ -7,6 +7,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMxvRlsW70GEkuqN/tdVM5X8EqdE6M/iA2iOHsA+HX8e sblatt@SS-Mac-sblatt.local" # MacBook
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJwbUnVjNdHpw4trfz2NEmg6Q901eO6cmELH2EilyFaV cbaxter@SS-Mac-cbaxter.local" # CJ
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIALkAoRXoHW+8AL7HTJQvwCFraLk3Fi7YtuB0N8yLpQ9 kentonb@SS-Mac-kentonb.local" # Kenton
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDsCrCpDvwG00kYONWjsKByp7c91+soPLcHhoqjzXleq dylanv3@SS-Mac-dylanv3.local" # Dylan
     ];
     shell = pkgs.shadow;
     home = "/var/empty";
