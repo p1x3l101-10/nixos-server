cd4f3926021fe1e1b82d058c527767d68595f452
Pixel <scott.blatt.0b10@gmail.com>
2025-03-05 22:59:13 -0600

    setup new pack

---
+++ modules/nixos/minecraft/fafoPack.nix
@@ -0,0 +1,44 @@
+{ pkgs, ... }:
+
+{
+  services.minecraft = {
+    enable = true;
+    curseforge = {
+      apiKey = import ./overrides/cfApiKey.nix;
+      mods = [
+        "projecte"
+        "projectextended"
+        "team-projecte"
+      ];
+    };
+    modrinth = {
+      mods = {
+        projects = [
+          "jei"
+          "emi"
+          "emi-ores"
+          "emi-loot"
+          "appleskin"
+        ];
+        downloadDependancies = "required";
+      };
+    };
+    settings = {
+      version = "1.20.1";
+      eula = true;
+      java.version = "21-alpine";
+      type = "neoforge";
+      memory = 8;
+      port = 25565;
+      openFirewall = true;
+      whitelist = import ./overrides/whitelist.nix;
+      rconStartup = [
+        "gamerule playersSleepingPercentage 10"
+      ];
+    };
+  };
+  # Persist server
+  environment.persistence."/nix/host/state/Servers/Minecraft/fafoPack".directories = [
+    { directory = "/var/lib/minecraft"; user = "1000"; group = "1000"; }
+  ];
+}
