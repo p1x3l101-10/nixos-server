6253afa3dc0b30dc115da4523e77cf505911019a
Pixel <scott.blatt.0b10@gmail.com>
2025-03-07 12:34:44 -0600

    add key

--- modules/nixos/server-base/proxy.nix
+++ modules/nixos/server-base/proxy.nix
@@ -8,6 +8,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJwbUnVjNdHpw4trfz2NEmg6Q901eO6cmELH2EilyFaV cbaxter@SS-Mac-cbaxter.local" # CJ
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIALkAoRXoHW+8AL7HTJQvwCFraLk3Fi7YtuB0N8yLpQ9 kentonb@SS-Mac-kentonb.local" # Kenton
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDsCrCpDvwG00kYONWjsKByp7c91+soPLcHhoqjzXleq dylanv3@SS-Mac-dylanv3.local" # Dylan
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIInfQpHlJseFgFIG8cedlMrWyjIgw08MaCbc00kn4lNx matthews7@SS-Mac-matthews7.local" # Spradley
     ];
     shell = pkgs.shadow;
     home = "/var/empty";
