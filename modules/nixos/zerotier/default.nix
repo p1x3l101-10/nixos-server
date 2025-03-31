41f8bab3ae3e82d42f6a8f544ce9e9f688991d4f
Pixel <scott.blatt.0b10@gmail.com>
2025-03-31 09:34:34 -0500

    persist

--- modules/nixos/zerotier/default.nix
+++ modules/nixos/zerotier/default.nix
@@ -5,4 +5,7 @@
     enable = true;
     joinNetworks = [ "272f5eae16a9e125" ];
   };
+  environment.persistence."/nix/host/state/Servers/ZeroTier".directories = [
+    { directory = "/var/lib/zerotier-one"; mode = "0700"; }
+  ];
 }
